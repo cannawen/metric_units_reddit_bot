f69281ffd81446579662a63678841daba773499d
Canna Wen <cannawen@gmail.com>
2017-07-28 17:15:00 +0200

    Making conversions generic

--- converter.js
+++ converter.js
@@ -1,13 +1,22 @@
+const regularExpressions = [{
+  "description" : "˚F to ˚C",
+  "regex" : /(-?\d+(?:\.\d*)?)˚F\b/g, 
+  "replacement" : (_, input) => Math.round(((input - 32) * 5/9)) + '˚C'
+}];
+
 function shouldConvert(input) {
-  return input.match(/(-?\d+(?:\.\d*)?)˚F\b/g) !== null;
+  for (var i = 0; i < regularExpressions.length; i++) {
+    if (input.match(regularExpressions[i]["regex"]) != null) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function convertString(input) {
-  function convert(str, p1, offset, input) {
-    return Math.round(((p1 - 32) * 5/9)) + '˚C';
-  }
-  var test = /(-?\d+(?:\.\d*)?)˚F\b/g;
-  return input.replace(test, convert);
+  return regularExpressions.reduce((memo, regex) => {
+    return memo.replace(regex["regex"], regex["replacement"])
+  }, input);
 }
 
 module.exports = {
