ec80bdfa78b01f60206bde0ec05bf6b35840b8ac
Canna Wen <cannawen@gmail.com>
2017-08-24 21:32:24 +0200

    Metric units human & capitalize in totallynotrobots sub [#150527705]

--- src/reply_maker.js
+++ src/reply_maker.js
@@ -50,20 +50,25 @@ const whatIsLove = {
 const stopMessage = "Please click 'block user' below and you will not see any more conversions from this bot.\n\nSo long, and thanks for all the fish";
 
 function formatReply(comment, conversions) {
-  return Object
+  const shouldBehaveHumanlike = comment['subreddit'] === "totallynotrobots";
+  const species = shouldBehaveHumanlike ? "human" : "bot"
+
+  const reply = Object
     .keys(conversions)
     .map(nonMetricValue => nonMetricValue + " | " + conversions[nonMetricValue])
     .join("  \n")
     + " "
-    + "^metric ^units ^bot" 
+    + "^metric ^units ^" + species 
     + " ^|"
-    + " ^[feedback](https://www.reddit.com/message/compose?to=cannawen&subject=metric%20units%20bot&message=I%20think%20your%20bot%20is...%20%5BPlease%20include%20a%20link%20if%20you%20are%20reporting%20a%20bug%20about%20a%20specific%20comment!%5D)"
+    + " ^[feedback](https://www.reddit.com/message/compose?to=cannawen&subject=metric%20units%20" + species + "&message=I%20think%20your%20" + species + "%20is...%20%5BPlease%20include%20a%20link%20if%20you%20are%20reporting%20a%20bug%20about%20a%20specific%20comment!%5D)"
     + " ^|"
     + " ^[source](https://github.com/cannawen/metric_units_reddit_bot)"
     + " ^|"
     + " ^[stop](https://www.reddit.com/message/compose?to=metric_units&subject=stop&message=If%20you%20would%20like%20to%20stop%20seeing%20this%20bot%27s%20comments%2C%20please%20send%20this%20private%20message%20with%20the%20subject%20%27stop%27.%20If%20you%20are%20a%20moderator%2C%20please%20go%20to%20https%3A%2F%2Fwww.reddit.com%2Fr%2F" + comment['subreddit'] + "%2Fabout%2Fbanned%2F)"
     + " ^|"
     + " ^" + environment['version'];
+
+  return shouldBehaveHumanlike ? reply.toUpperCase() : reply;
 }
 
 module.exports = {
--- test/reply_maker-test.js
+++ test/reply_maker-test.js
@@ -6,13 +6,13 @@ const replier = require('../src/reply_maker');
 describe('reply_maker', () => {
   describe('#formatReply()', () => {
     it('should create a tabular response for 1 conversion', () => {
-      replier.formatReply("Test foo", {"foo" : "bar"})
+      replier.formatReply({ 'subreddit': 'all' }, {"foo" : "bar"})
         .should
         .include("foo | bar ");
     });
 
     it('should create tabular response for 2 conversions', () => {
-      replier.formatReply("Hello there foo how are you meep merp Hello there!", {"foo" : "bar", "hi" : "hey"})
+      replier.formatReply({ 'subreddit': 'all' }, {"foo" : "bar", "hi" : "hey"})
         .should
         .include("foo | bar  \n")
         .and
@@ -20,13 +20,25 @@ describe('reply_maker', () => {
     });
 
     it('should create tabular response of supersets of text', () => {
-      replier.formatReply("Test foobar foo cat cat", {"foo" : "bar", "foobar" : "hey", "cat":"dog"})
+      replier.formatReply({ 'subreddit': 'all' }, {"foo" : "bar", "foobar" : "hey", "cat":"dog"})
         .should
         .include("foo | bar  \n")
         .and
         .include("foobar | hey  \n")
         .and
         .include("cat | dog ");
+    });
+
+    it('should say metric units bot on normal subreddits', () => {
+      replier.formatReply({ 'subreddit': 'all' }, {})
+        .should
+        .include("^metric ^units ^bot")
+    });
+
+    it('should say metric units human on totallynotrobots', () => {
+      replier.formatReply({ 'subreddit': 'totallynotrobots' }, {})
+        .should
+        .include("^METRIC ^UNITS ^HUMAN")
     });
   });
 });
