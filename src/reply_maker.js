2382d8c2c361bdb00d7ca0c6942116ed690c565e
Canna Wen <cannawen@gmail.com>
2017-08-26 21:02:35 +0200

    Make more human-like response for totallynotrobots

--- src/reply_maker.js
+++ src/reply_maker.js
@@ -90,8 +90,19 @@ const whatIsLove = {
 const stopMessage = "Please click 'block user' below and you will not see any more conversions from this bot.\n\nSo long, and thanks for all the fish";
 
 function formatReply(comment, conversions) {
-  const shouldBehaveHumanlike = comment['subreddit'].match(/^totallynotrobots$/i);
-  const species = shouldBehaveHumanlike ? "human" : "bot"
+  let species = "bot"
+  let source = "source"
+  let version = environment['version'];
+  let subreddit = comment['subreddit'];
+  let uppercase = false;
+
+  if (comment['subreddit'].match(/^totallynotrobots$/i)) {
+    species = "~~bot~~ ^human";
+    source =  "~~source~~";
+    version = "~~" + environment['version'] + "~~";
+    subreddit = "I_AM_HUMAN_DO_NOT_BAN"
+    uppercase = true;
+  } 
 
   const reply = Object
     .keys(conversions)
@@ -102,13 +113,13 @@ function formatReply(comment, conversions) {
     + " ^|"
     + " ^[feedback](https://www.reddit.com/message/compose?to=cannawen&subject=metric%20units%20" + species + "&message=I%20think%20your%20" + species + "%20is...%20%5BPlease%20include%20a%20link%20if%20you%20are%20reporting%20a%20bug%20about%20a%20specific%20comment!%5D)"
     + " ^|"
-    + " ^[source](https://github.com/cannawen/metric_units_reddit_bot)"
+    + " ^[" + source + "](https://github.com/cannawen/metric_units_reddit_bot)"
     + " ^|"
-    + " ^[stop](https://www.reddit.com/message/compose?to=metric_units&subject=stop&message=If%20you%20would%20like%20to%20stop%20seeing%20this%20bot%27s%20comments%2C%20please%20send%20this%20private%20message%20with%20the%20subject%20%27stop%27.%20If%20you%20are%20a%20moderator%2C%20please%20go%20to%20https%3A%2F%2Fwww.reddit.com%2Fr%2F" + comment['subreddit'] + "%2Fabout%2Fbanned%2F)"
+    + " ^[stop](https://www.reddit.com/message/compose?to=metric_units&subject=stop&message=If%20you%20would%20like%20to%20stop%20seeing%20this%20bot%27s%20comments%2C%20please%20send%20this%20private%20message%20with%20the%20subject%20%27stop%27.%20If%20you%20are%20a%20moderator%2C%20please%20go%20to%20https%3A%2F%2Fwww.reddit.com%2Fr%2F" + subreddit + "%2Fabout%2Fbanned%2F)"
     + " ^|"
-    + " ^" + environment['version'];
+    + " ^" + version;
 
-  return shouldBehaveHumanlike ? reply.toUpperCase() : reply;
+  return uppercase ? reply.toUpperCase() : reply;
 }
 
 module.exports = {
